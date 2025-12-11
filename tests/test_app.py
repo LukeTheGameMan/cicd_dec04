b7dfcb1abb31ad0de034d537f857717a7a02ad73
LukeTheGameMan <116319182+LukeTheGameMan@users.noreply.github.com>
2025-12-10 21:36:31 -0600

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,7 +5,7 @@ root = Path(__file__).resolve().parents[1]
 sys.path.insert(0, str(root / "src"))
 
 
-from app import add, sub, mult, div, log
+from app import add, sub, mult, div, log, square, sin, cos, sqrt, perc
 
 def test_add():
     assert add(5, 6) == 11
@@ -44,3 +44,21 @@ def test_log2():
 def test_log3():
     with pytest.raises(Exception):
         log(-2, 0)
+
+def test_square1():
+    assert square(3) == 9
+
+def test_square2():
+    assert square(3) != 10
+
+def test_sin1():
+    assert sin((math.pi)/2) == 1
+
+def test_sin2():
+    assert sin(90, True) == 1
+
+def test_sin3():
+    assert sin((math.pi)/2) != 0
+
+def test_sin4():
+    assert sin(90, True) != 0
